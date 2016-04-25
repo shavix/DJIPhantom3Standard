9b37f3d3d8069d177fff312bb6b67516a70a3768
shavix <davidpr1994@gmail.com>
2016-04-24 18:25:20 -0700

    5 controls

--- DJIPhantom/CaptureViewController.m
+++ DJIPhantom/CaptureViewController.m
@@ -16,17 +16,19 @@
 #define weakSelf(__TARGET__) __weak typeof(self) __TARGET__=self
 #define weakReturn(__TARGET__) if(__TARGET__==nil)return;
 
+float rollAngle = 10;
+float yawAngle = 10;
+
 @interface CaptureViewController () <DJICameraDelegate, DJISDKManagerDelegate, DJIPlaybackDelegate, DJIFlightControllerDelegate>
 
 @property (strong, nonatomic) UIImageView *backgroundImageView;
 @property (strong, nonatomic) UIView *fpvPreviewView;
-// sliders
-@property (strong, nonatomic) UISlider *leftXSlider;
-@property (strong, nonatomic) UISlider *leftYSlider;
-@property (strong, nonatomic) UISlider *rightXSlider;
-@property (strong, nonatomic) UISlider *rightYSlider;
-// temp button
-@property (strong, nonatomic) UIButton *button;
+@property (strong, nonatomic) UIButton *landButton;
+@property (strong, nonatomic) UIButton *rotateRightButton;
+@property (strong, nonatomic) UIButton *rotateLeftButton;
+@property (strong, nonatomic) UIButton *rollRightButton;
+@property (strong, nonatomic) UIButton *rollLeftButton;
+
 
 @end
 
@@ -47,50 +49,77 @@
     [self.view addSubview:self.backgroundImageView];
     
     // imageView
-    CGFloat width = self.view.frame.size.width / 2;
+    CGFloat width = self.view.frame.size.width / 3;
     CGFloat height = 3 * self.view.frame.size.height / 4;
-    CGRect frame = CGRectMake(width / 2, 0, width, height);
+    CGRect frame = CGRectMake(width, 0, width, height);
     self.fpvPreviewView = [[UIView alloc] initWithFrame:frame];
+    self.fpvPreviewView.backgroundColor = [UIColor blackColor];
     [self.view addSubview:self.fpvPreviewView];
- 
-    [self setupSliders];
     
-    // temp button
-    self.button = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.button.frame = CGRectMake(self.view.frame.size.width / 3, self.view.frame.size.height - 60, 100, 40);
-    self.button.backgroundColor = [UIColor whiteColor];
+    [self setupButtons];
+    
+}
+
+- (void)setupButtons {
+    
+    // autoLand button
+    self.landButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _landButton.frame = CGRectMake(3 * self.view.frame.size.width / 8, self.view.frame.size.height - 60, self.view.frame.size.width / 4, 40);
+    [_landButton setTitle:@"Land" forState:UIControlStateNormal];
+    [_landButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    _landButton.backgroundColor = [UIColor whiteColor];
     // Add an action in current code file (i.e. target)
-    [_button addTarget:self action:@selector(buttonPressed:)
-     forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:_button];
-}
-
-- (void)buttonPressed:(id)sender {
+    [_landButton addTarget:self action:@selector(landButtonPressed:)
+      forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_landButton];
     
-    [self rotateDroneWithJoystick];
+    CGFloat width = self.view.frame.size.width / 4;
+    CGFloat height = self.view.frame.size.height / 3;
     
-}
-
-- (void)setupSliders {
+    // rotate left button
+    self.rotateLeftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _rotateLeftButton.frame = CGRectMake(10, 10, width, height);
+    [_rotateLeftButton setTitle:@"L Rotate" forState:UIControlStateNormal];
+    [_rotateLeftButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    _rotateLeftButton.backgroundColor = [UIColor whiteColor];
+    // Add an action in current code file (i.e. target)
+    [_rotateLeftButton addTarget:self action:@selector(lRotatePressed:)
+          forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_rotateLeftButton];
     
-    CGFloat width = self.view.frame.size.width;
-    CGFloat height = 10;
-    CGFloat y = self.view.frame.size.height - 60;
+    // rotate right button
+    self.rotateRightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _rotateRightButton.frame = CGRectMake(10, 20 + height, width, height);
+    [_rotateRightButton setTitle:@"R Rotate" forState:UIControlStateNormal];
+    [_rotateRightButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    _rotateRightButton.backgroundColor = [UIColor whiteColor];
+    // Add an action in current code file (i.e. target)
+    [_rotateRightButton addTarget:self action:@selector(rRotatePressed:)
+          forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_rotateRightButton];
     
-    CGRect leftXFrame = CGRectMake(20, y, width/4, height);
-    CGRect leftYFrame = CGRectMake(20, y, width/4, height);
-    CGRect rightXFrame = CGRectMake(width - width/4 - 20, y, width/4, height);
-    CGRect rightYFrame = CGRectMake(width - width/4 - 20, y, width/4, height);
+    // roll left button
+    self.rollLeftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _rollLeftButton.frame = CGRectMake(self.view.frame.size.width - width - 10, 10, width, height);
+    [_rollLeftButton setTitle:@"L Roll" forState:UIControlStateNormal];
+    [_rollLeftButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    _rollLeftButton.backgroundColor = [UIColor whiteColor];
+    // Add an action in current code file (i.e. target)
+    [_rollLeftButton addTarget:self action:@selector(lRotatePressed:)
+                 forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_rollLeftButton];
     
-    self.leftXSlider = [[UISlider alloc] initWithFrame:leftXFrame];
-    self.leftYSlider = [[UISlider alloc] initWithFrame:leftYFrame];
-    self.rightXSlider = [[UISlider alloc] initWithFrame:rightXFrame];
-    self.rightYSlider = [[UISlider alloc] initWithFrame:rightYFrame];
     
-    [self.view addSubview:_leftXSlider];
-    //[self.view addSubview:_leftYSlider];
-    [self.view addSubview:_rightXSlider];
-    //[self.view addSubview:_rightYSlider];
+    // roll right button
+    self.rollRightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _rollRightButton.frame = CGRectMake(self.view.frame.size.width - width - 10, 20 + height, width, height);
+    [_rollRightButton setTitle:@"R Roll" forState:UIControlStateNormal];
+    [_rollRightButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    _rollRightButton.backgroundColor = [UIColor whiteColor];
+    // Add an action in current code file (i.e. target)
+    [_rollRightButton addTarget:self action:@selector(rRollPressed:)
+               forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_rollRightButton];
     
 }
 
@@ -108,38 +137,165 @@
 
 }
 
-#pragma mark - flightController
+#pragma mark - buttons
 
-- (DJIFlightController*) fetchFlightController {
-    if (![DJISDKManager product]) {
-        return nil;
+- (void)landButtonPressed:(id)sender {
+    
+    DJIFlightController *flightController = [self fetchFlightController];
+    
+    [flightController autoLandingWithCompletion:^(NSError *error){
+        NSLog(@"landed");
+    }];
+}
+
+- (void)lRotatePressed:(id)sender {
+    
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(rotateDrone:) userInfo:@{@"Direction":@"l"} repeats:NO];
+    [timer fire];
+    
+    [[NSRunLoop currentRunLoop]addTimer:timer forMode:NSDefaultRunLoopMode];
+    [[NSRunLoop currentRunLoop]runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
+    
+    [timer invalidate];
+    timer = nil;
+
+    
+}
+
+- (void)rRotatePressed:(id)sender {
+    
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(rotateDrone:) userInfo:@{@"Direction":@"r"} repeats:NO];
+    [timer fire];
+    
+    [[NSRunLoop currentRunLoop]addTimer:timer forMode:NSDefaultRunLoopMode];
+    [[NSRunLoop currentRunLoop]runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
+    
+    [timer invalidate];
+    timer = nil;
+
+    
+}
+
+- (void)lRollPressed:(id)sender {
+    
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(rollDrone:) userInfo:@{@"Direction":@"l"} repeats:NO];
+    [timer fire];
+    
+    [[NSRunLoop currentRunLoop]addTimer:timer forMode:NSDefaultRunLoopMode];
+    [[NSRunLoop currentRunLoop]runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
+    
+    [timer invalidate];
+    timer = nil;
+
+    
+}
+
+- (void)rRollPressed:(id)sender {
+    
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(rollDrone:) userInfo:@{@"Direction":@"r"} repeats:NO];
+    [timer fire];
+    
+    [[NSRunLoop currentRunLoop]addTimer:timer forMode:NSDefaultRunLoopMode];
+    [[NSRunLoop currentRunLoop]runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
+    
+    [timer invalidate];
+    timer = nil;
+    
+}
+
+#pragma mark - DJIFlightController
+
+
+- (void)rollDrone:(NSTimer *)timer {
+    
+    NSDictionary *dict = [timer userInfo];
+    NSString *direction = [dict objectForKey:@"Direction"];
+    
+    float angle = rollAngle;
+    if([direction isEqualToString:@"l"]) {
+        angle = angle*-1;
     }
-    if ([[DJISDKManager product] isKindOfClass:[DJIAircraft class]]) {
-        return ((DJIAircraft*)[DJISDKManager product]).flightController;
+    
+    NSLog(@"%f", angle);
+    
+    DJIFlightController *flightController = [self fetchFlightController];
+    
+    DJIVirtualStickFlightControlData vsFlightCtrlData;
+    vsFlightCtrlData.pitch = 0;
+    vsFlightCtrlData.roll = angle;
+    vsFlightCtrlData.verticalThrottle = 0;
+    vsFlightCtrlData.yaw = 0;
+    
+    [flightController sendVirtualStickFlightControlData:vsFlightCtrlData withCompletion:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Send FlightControl Data Failed %@", error.description);
+        }
+    }];
+    
+}
+
+- (void)rotateDrone:(NSTimer *)timer
+{
+    NSDictionary *dict = [timer userInfo];
+    NSString *direction = [dict objectForKey:@"Direction"];
+    
+    float angle = yawAngle;
+    if([direction isEqualToString:@"l"]) {
+        angle = angle*-1;
     }
-    return nil;
+    NSLog(@"%f", angle);
+   
+    DJIFlightController *flightController = [self fetchFlightController];
+    
+    DJIVirtualStickFlightControlData vsFlightCtrlData;
+    vsFlightCtrlData.pitch = 0;
+    vsFlightCtrlData.roll = 0;
+    vsFlightCtrlData.verticalThrottle = 0;
+    vsFlightCtrlData.yaw = angle;
+    
+    [flightController sendVirtualStickFlightControlData:vsFlightCtrlData withCompletion:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Send FlightControl Data Failed %@", error.description);
+        }
+    }];
+    
 }
 
 
-- (void)sendVirtualStickFlightControlData:(DJIVirtualStickFlightControlData)controlData withCompletion:(DJICompletionBlock)completion {
+
+
+- (void)rotateDroneWithJoystick
+{
     
-    NSLog(@"%f", controlData.yaw);
+    
+    /*
+     int iterations = 1;
+     
+     for(int i = 0;i < iterations; i++){
+     
+     float yawAngle = 90*i;
+     float rollAngle = 10;
+     
+     if (yawAngle > DJIVirtualStickYawControlMaxAngle) { //Filter the angle between -180 ~ 0, 0 ~ 180
+     yawAngle = yawAngle - 360;
+     }
+     
+     //NSTimer *timer =  [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(rotateDrone:) userInfo:@{@"YawAngle":@(yawAngle)} repeats:NO];
+     NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(rollDrone:) userInfo:@{@"RollAngle":@(rollAngle)} repeats:NO];
+     [timer fire];
+     
+     [[NSRunLoop currentRunLoop]addTimer:timer forMode:NSDefaultRunLoopMode];
+     [[NSRunLoop currentRunLoop]runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.2]];
+     
+     [timer invalidate];
+     timer = nil;
+     }
+     */
+    
     
 }
 
-#pragma mark - Camera
-
-- (DJICamera*) fetchCamera {
-    
-    if (![DJISDKManager product]) {
-        return nil;
-    }
-    if ([[DJISDKManager product] isKindOfClass:[DJIAircraft class]]) {
-        return ((DJIAircraft*)[DJISDKManager product]).camera;
-    }
-    return nil;
-}
-
+#pragma mark - DJICamera
 
 -(void) sdkManagerProductDidChangeFrom:(DJIBaseProduct* _Nullable) oldProduct to:(DJIBaseProduct* _Nullable) newProduct
 {
@@ -164,58 +320,6 @@
     
 }
 
-
-- (void)playbackManager:(DJIPlaybackManager *)playbackManager didUpdatePlaybackState:(DJICameraPlaybackState *)playbackState {
-    
-    
-    
-}
-
-- (void)rotateDroneWithJoystick
-{
-    
-    for(int i = 0;i < 1; i++){
-        
-        float yawAngle = 5*i;
-        
-        if (yawAngle > DJIVirtualStickYawControlMaxAngle) { //Filter the angle between -180 ~ 0, 0 ~ 180
-            yawAngle = yawAngle - 360;
-        }
-        
-        NSTimer *timer =  [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(rotateDrone:) userInfo:@{@"YawAngle":@(yawAngle)} repeats:YES];
-        [timer fire];
-        
-        [[NSRunLoop currentRunLoop]addTimer:timer forMode:NSDefaultRunLoopMode];
-        [[NSRunLoop currentRunLoop]runUntilDate:[NSDate dateWithTimeIntervalSinceNow:2]];
-        
-        [timer invalidate];
-        timer = nil;
-    }
-    
-}
-
-- (void)rotateDrone:(NSTimer *)timer
-{
-    NSDictionary *dict = [timer userInfo];
-    float yawAngle = [[dict objectForKey:@"YawAngle"] floatValue];
-    
-    DJIFlightController *flightController = [self fetchFlightController];
-    
-    DJIVirtualStickFlightControlData vsFlightCtrlData;
-    vsFlightCtrlData.pitch = 0;
-    vsFlightCtrlData.roll = 0;
-    vsFlightCtrlData.verticalThrottle = 0;
-    vsFlightCtrlData.yaw = yawAngle;
-    
-    [flightController sendVirtualStickFlightControlData:vsFlightCtrlData withCompletion:^(NSError * _Nullable error) {
-        if (error) {
-            NSLog(@"Send FlightControl Data Failed %@", error.description);
-        }
-    }];
-    
-}
-
-#pragma mark - DJICameraDelegate Method
 -(void)camera:(DJICamera *)camera didReceiveVideoData:(uint8_t *)videoBuffer length:(size_t)size
 {
     uint8_t* pBuffer = (uint8_t*)malloc(size);
@@ -225,7 +329,21 @@
 
 - (void)camera:(DJICamera *)camera didUpdateSystemState:(DJICameraSystemState *)systemState
 {
+    
 }
+
+
+- (DJICamera*) fetchCamera {
+    
+    if (![DJISDKManager product]) {
+        return nil;
+    }
+    if ([[DJISDKManager product] isKindOfClass:[DJIAircraft class]]) {
+        return ((DJIAircraft*)[DJISDKManager product]).camera;
+    }
+    return nil;
+}
+
 
 #pragma mark - DJISDKManager
 
@@ -252,6 +370,34 @@
     [alert addAction:okAction];
     [self presentViewController:alert animated:YES completion:nil];
 }
+
+
+#pragma mark - flightController
+
+- (DJIFlightController*) fetchFlightController {
+    if (![DJISDKManager product]) {
+        return nil;
+    }
+    if ([[DJISDKManager product] isKindOfClass:[DJIAircraft class]]) {
+        return ((DJIAircraft*)[DJISDKManager product]).flightController;
+    }
+    return nil;
+}
+
+
+- (void)sendVirtualStickFlightControlData:(DJIVirtualStickFlightControlData)controlData withCompletion:(DJICompletionBlock)completion {
+    
+    NSLog(@"%f", controlData.yaw);
+    
+}
+
+
+#pragma mark - DJIPlaybackManager
+
+- (void)playbackManager:(DJIPlaybackManager *)playbackManager didUpdatePlaybackState:(DJICameraPlaybackState *)playbackState {
+    
+}
+
 
 
 @end
